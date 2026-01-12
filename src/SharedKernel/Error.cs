9770da5c1dfb4fae4e897c03b366ffb30854fd26
ceemeese <cmsalvador92@gmail.com>
2026-01-12 11:30:56 +0100

    Añade Scrutor, tipo Error y ErrorType

---
+++ src/SharedKernel/Error.cs
@@ -0,0 +1,33 @@
+namespace SharedKernel;
+
+public record Error
+{
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+    
+    public Error(string code, string description, ErrorType type)
+    {
+        Code = code;
+        Description = description;
+        Type = type;
+    }
+    
+    public string Code { get; }
+    public string Description { get; }
+    public ErrorType Type { get; }
+
+    
+    public static Error Failure(string code, string description) =>
+        new Error(code, description, ErrorType.Failure);
+    
+    public static Error Validation(string code, string description) =>
+        new Error(code, description, ErrorType.Validation);
+    
+    public static Error Problem(string code, string description) =>
+        new Error(code, description, ErrorType.Problem);
+    
+    public static Error NotFound(string code, string description) =>
+        new Error(code, description, ErrorType.NotFound);
+    
+    public static Error Conflict(string code, string description) =>
+        new Error(code, description, ErrorType.Conflict);
+}
