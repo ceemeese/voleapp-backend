7cea3d6c7c45e28082888d7ef54ebde37d6a6910
ceemeese <cmsalvador92@gmail.com>
2026-01-13 13:00:16 +0100

    [VOL-43] - Logging decorator en pipeline para registrar inicio de logs en peticiones

---
+++ src/Application/Abstractions/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,37 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
+using SharedKernel;
+
+namespace Application.Abstractions.Behaviors;
+
+internal sealed class LoggingBehavior<TRequest, TResponse> (
+    ILogger<LoggingBehavior<TRequest, TResponse>> logger) 
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : class where TResponse : Result
+{
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        string requestName = typeof(TRequest).Name;
+        
+        logger.LogInformation("Iniciando petición {requestName}", requestName);
+
+        //delegado
+        TResponse response = await next();
+
+        if (response.IsSuccess)
+        {
+            logger.LogInformation("Petición completada {requestName}", requestName);
+        }
+        else
+        {
+            using (LogContext.PushProperty("Error", response.Error, true))
+            {
+                logger.LogError("Petición completada con errores {requestName}", requestName);
+            }
+        }
+        
+        return response;
+    }
+}
